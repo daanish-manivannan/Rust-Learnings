2fd33f8becc2871a270448b61ea02555fe83d303
Daanish Manivannan <daanish2721@gmail.com>
2026-02-19 16:31:39 +0000

    Variable Mut and immutable keyword

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ fn main() {
     println!("Hello, world!");
     let y=10;
     let mut x = 4;
-    println!("The value of x is: {}", x);
+    println!("The value of x is: {} and y is: {}", x, y);
 
 }
